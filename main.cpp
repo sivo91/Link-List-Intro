97b31b311e5c4ceee663442c20ec7d37c64ca6d2
Peter <83480296+sivo91@users.noreply.github.com>
2023-10-04 22:15:45 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,3 +48,54 @@ int main() {
  14
  35
 */
+
+
+
+*************************** next example ************************************
+
+
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node* Link;
+};
+
+void printList(Node* n)
+{
+    while (n != NULL) {
+        cout << n->value << endl;
+        n = n->Link;
+    }
+}
+
+int main() {
+
+
+    int Arr[] = { 4, 6, 3, 7, 55, 23, 63 };
+    int size = sizeof(Arr) / sizeof(Arr[0]);  // 28 / 4
+
+    cout << "sizeof Arr " << sizeof(Arr) << endl;
+    cout << "sizeOf Arr[0] " << sizeof(Arr[0]) << endl;
+
+    cout << "size of Arr[] " <<  size << endl; // 7
+
+
+    const int SIZE = 7;
+    Node nodes[SIZE];  // Declare an array of Node objects
+
+    int values[] = { 4, 6, 3, 7, 55, 23, 63 };  // Values to be assigned to nodes
+
+    for (int i = 0; i < SIZE; i++) {
+        nodes[i].value = values[i];
+        nodes[i].Link = (i < SIZE - 1) ? &nodes[i + 1] : NULL;
+    }
+
+    printList(nodes);  // Pass the address of the first node in the array
+
+    return 0;
+}
+    
